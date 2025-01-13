3329f7e1dcbacd6429f1bcad2dfc531d77da7d6d
zpz999 <1443437972@qq.com>
2025-01-13 10:57:08 +0800

    routes.py

--- src/optinetsim_backend/app/routes.py
+++ src/optinetsim_backend/app/routes.py
@@ -4,7 +4,9 @@ from flask_restful import Api
 # Project imports
 from src.optinetsim_backend.app.auth import *
 from src.optinetsim_backend.app.database import *
-
+from src.optinetsim_backend.app.database.global_config import SimulationConfigResource
+from src.optinetsim_backend.app.database.global_config import SpectrumInformationResource
+from src.optinetsim_backend.app.database.global_config import SpanParametersResource
 
 def api_init_app(app):
     api = Api(app)
@@ -33,7 +35,10 @@ def api_init_app(app):
                      '/api/equipment-libraries/<string:library_id>/equipment/<string:category>/<string:type_variety>')
     api.add_resource(EquipmentDeleteResource,
                      '/api/equipment-libraries/<string:library_id>/equipment/<string:category>/<string:type_variety>')
-
+    #全局变量相关接口
+    api.add_resource(SimulationConfigResource, '/api/networks/<string:network_id>/simulation-config')
+    api.add_resource(SpectrumInformationResource, '/api/networks/<string:network_id>/spectrum-information')
+    api.add_resource(SpanParametersResource, '/api/networks/<string:network_id>/span-parameters')
     # 仿真相关接口
     # TODO: API resource for simulation
 
