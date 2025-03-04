a89dd7e63ec0c8a13f579532e87fd68ba82b5f14
zpz999 <1443437972@qq.com>
2025-03-04 21:28:02 +0800

    network.py

--- src/optinetsim_backend/app/database/network.py
+++ src/optinetsim_backend/app/database/network.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 # Project imports
-from src.optinetsim_backend.app.database.models import NetworkDB
+from src.optinetsim_backend.app.database.models import NetworkDB, convert_objectid_and_datetime
 
 
 class NetworkList(Resource):
@@ -31,22 +31,16 @@ class NetworkList(Resource):
 
 
 class NetworkResource(Resource):
-    @jwt_required()  # 添加 JWT 鉴权
+    @jwt_required()
     def get(self, network_id):
         user_id = get_jwt_identity()
-        networks = NetworkDB.find_by_network_id(user_id, network_id)
-        # ObjectId 转换为字符串
-        networks['_id'] = str(networks['_id'])
-        networks['user_id'] = str(networks['user_id'])
-        # 时间格式转换
-        networks['created_at'] = networks['created_at'].strftime('%Y-%m-%dT%H:%M:%SZ')
-        networks['updated_at'] = networks['updated_at'].strftime('%Y-%m-%dT%H:%M:%SZ')
-        if networks:
-            # 返回网络信息
-            return networks, 200
+        network = NetworkDB.find_by_network_id(user_id, network_id)
+        if network:
+            network = convert_objectid_and_datetime(network)
+            return network, 200
         return {'message': 'Network not found'}, 404
 
-    @jwt_required()  # 添加 JWT 鉴权
+    @jwt_required()
     def put(self, network_id):
         user_id = get_jwt_identity()
         parser = reqparse.RequestParser()
@@ -55,19 +49,19 @@ class NetworkResource(Resource):
 
         network = NetworkDB.modify_network_name(user_id, network_id, args['network_name'])
         if network:
-            # 返回网络信息
+            network = convert_objectid_and_datetime(network)
             return {
-                "network_id": str(network['_id']),
+                "network_id": network['_id'],
                 "network_name": network['network_name'],
-                "created_at": network['created_at'].strftime('%Y-%m-%dT%H:%M:%SZ'),
-                "updated_at": network['updated_at'].strftime('%Y-%m-%dT%H:%M:%SZ')
+                "created_at": network['created_at'],
+                "updated_at": network['updated_at']
             }, 200
         return {'message': 'Network not found'}, 404
 
-    @jwt_required()  # 添加 JWT 鉴权
+    @jwt_required()
     def delete(self, network_id):
         user_id = get_jwt_identity()
-        network = NetworkDB.delete_by_network_id(user_id, network_id)
-        if network:
+        result = NetworkDB.delete_by_network_id(user_id, network_id)
+        if result:
             return {'message': 'Network deleted successfully'}, 200
         return {'message': 'Network not found'}, 404
