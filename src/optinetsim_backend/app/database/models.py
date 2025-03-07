89bedda871be49f9fef71ed923dd15c752587de8
zeroSignal <616201092@qq.com>
2025-03-07 20:49:42 +0800

    Fix equipment library api.

--- src/optinetsim_backend/app/database/models.py
+++ src/optinetsim_backend/app/database/models.py
@@ -229,8 +229,7 @@ class EquipmentLibraryDB:
 
     @staticmethod
     def delete(library_id):
-        result = db.equipment_libraries.delete_one({"_id": ObjectId(library_id)})
-        return result.deleted_count > 0
+        return db.equipment_libraries.delete_one({"_id": ObjectId(library_id)})
 
     @staticmethod
     def delete_by_user_id(user_id):
@@ -239,57 +238,33 @@ class EquipmentLibraryDB:
     # 新增器件的方法
     @staticmethod
     def add_equipment(library_id, category, equipment):
-        library = db.equipment_libraries.find_one({"_id": ObjectId(library_id)})
-
-        if not library or category not in library['equipments']:
-            return False
-
         # 检查该类别下是否已经存在相同类型的器件
-        existing_equipment = next(
-            (e for e in library['equipments'][category] if e['type_variety'] == equipment['type_variety']),
-            None
+        res = db.equipment_libraries.find_one(
+            {"_id": ObjectId(library_id), f"equipments.{category}.type_variety": equipment['type_variety']}
         )
-
-        # 如果已存在相同的器件，返回 False
-        if existing_equipment:
+        if res:
             return False
 
         # 如果没有重复，添加器件到该类别
-        library['equipments'][category].append(equipment)
-
-        # 更新器件库
         db.equipment_libraries.update_one(
             {"_id": ObjectId(library_id)},
-            {"$set": {"equipments": library['equipments'], "updated_at": datetime.utcnow()}}
+            {"$push": {f"equipments.{category}": equipment}}
         )
         return True
 
     # 更新器件的方法
     @staticmethod
     def update_equipment(library_id, category, type_variety, equipment_update):
-        library = db.equipment_libraries.find_one({"_id": ObjectId(library_id)})
-        if library and category in library['equipments']:
-            equipment = next((e for e in library['equipments'][category] if e['type_variety'] == type_variety), None)
-            if equipment:
-                equipment = equipment_update
-                db.equipment_libraries.update_one(
-                    {"_id": ObjectId(library_id)},
-                    {"$set": {"equipments": library['equipments'], "updated_at": datetime.utcnow()}}
-                )
-                return True
-        return False
+        # 更新器件信息
+        db.equipment_libraries.update_one(
+            {"_id": ObjectId(library_id), f"equipments.{category}.type_variety": type_variety},
+            {"$set": {f"equipments.{category}.$": equipment_update}}
+        )
 
     # 删除器件的方法
     @staticmethod
     def delete_equipment(library_id, category, type_variety):
-        library = db.equipment_libraries.find_one({"_id": ObjectId(library_id)})
-        if library and category in library['equipments']:
-            equipment = next((e for e in library['equipments'][category] if e['type_variety'] == type_variety), None)
-            if equipment:
-                library['equipments'][category].remove(equipment)
-                db.equipment_libraries.update_one(
-                    {"_id": ObjectId(library_id)},
-                    {"$set": {"equipments": library['equipments'], "updated_at": datetime.utcnow()}}
-                )
-                return True
-        return False
+        return db.equipment_libraries.update_one(
+            {"_id": ObjectId(library_id)},
+            {"$pull": {f"equipments.{category}": {"type_variety": type_variety}}}
+        )
